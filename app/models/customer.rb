96a16c0129a67dde0598d8af7f5b8f1d112843d9
Fernanda Andrade <fernandaao@gmail.com>
2021-06-30 07:42:46 -0300

    valitations and enums

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+
+  validates :name, presence: true
+  enum status: [:active, :inactive, :blocked]
+  enum authorization: [:regular, :admin, :operator] 
 end
