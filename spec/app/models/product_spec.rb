ea4a5f78610dc8ced664ad999c1552fa7c839bc6
Emily Faccin <emily.faccin@gmail.com>
2021-07-05 07:48:52 -0300

    add: validation and tests to Product

---
+++ spec/app/models/product_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  let(:category) { create(:category) }
+  subject { build(:product, category: category) }
+
+  context 'validations' do
+    it { is_expected.to belong_to(:category) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to have_db_index(:name) }
+    it do
+      is_expected.to define_enum_for(:status).
+        with_values([:active, :inactive])
+    end
+  end
+end
