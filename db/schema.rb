5efb486691099cfac8733a5ce0e56df72a0bbf0f
Emily Faccin <emily.faccin@gmail.com>
2021-07-05 07:48:52 -0300

    add: validation and tests to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_093414) do
+ActiveRecord::Schema.define(version: 2021_07_05_101209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_05_093414) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "customers", force: :cascade do |t|
--- spec/models/product_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Product, type: :model do
-  context 'validations' do
-    it { is_expected.to belong_to(:category) }
-    it { is_expected.to validate_presence_of(:name) }
-  end
-end
