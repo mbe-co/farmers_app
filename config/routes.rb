3cdafa84ae6050857154c3602d24a9020e4bce97
Fernanda Andrade <fernandaao@gmail.com>
2021-07-05 21:23:31 -0300

    i did a lot of things, sir

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)", :locale => /pt-BR|en|fr|es/ do
     root to: 'pages#index'
     devise_for :customers
+    namespace :backoffice do
+      resources :categories, except: [:show]
+    end
   end
 end
