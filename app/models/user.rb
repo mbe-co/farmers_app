a01fbf4e9800a26506c0db507eeb99c17084216b
Emily Faccin <emily.faccin@gmail.com>
2021-07-08 07:58:21 -0300

    removes confirmable from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
 
   validates :email, :name, presence: true
   validates :email, uniqueness: true
