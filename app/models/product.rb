d712b4d462c221e9cc98a1e61f3e85046d9ce9d5
Emily Faccin <emily.faccin@gmail.com>
2021-07-11 15:25:50 -0300

    refactor: translation of product's status using a method

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,9 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  enum status: [:active, :inactive]
+  enum status: { active: 0, inactive: 1 }
+
+  def status_name
+    Product.human_attribute_name("status.#{status}")
+  end
 end
