412d94ba561fce73281577ff771ac56d05548abd
Emily Faccin <emily.faccin@gmail.com>
2021-07-12 07:28:36 -0300

    add: default value to product status

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  enum status: { active: 0, inactive: 1 }
+  enum status: { active: 0, inactive: 1 }, _default: 0
 
   def status_name
     Product.human_attribute_name("status.#{status}")
