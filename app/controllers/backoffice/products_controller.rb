66de5fd3729184f6a8281daf07468b5603241389
Emily Faccin <emily.faccin@gmail.com>
2021-07-12 07:27:56 -0300

    add: insertion feature on products

--- app/controllers/backoffice/products_controller.rb
+++ app/controllers/backoffice/products_controller.rb
@@ -2,8 +2,36 @@ module Backoffice
   class ProductsController < ApplicationController
     include AdminsOnly
 
+    before_action :find_category, only: [:create]
+
     def index
       @products = Product.all
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = @category.products.build(product_params)
+
+      if @product.save
+        redirect_to backoffice_products_path, notice: t('messages.success_on_create')
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product)
+            .permit(:name, :category, :status)
+            .except(:category)
+    end
+
+    def find_category
+      @category = Category.find(params.dig(:product, :category))
     end
   end
 end
