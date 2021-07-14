3dabb75cf41ce1e5efc747fb87252ac5f4b0b7cb
Emily Faccin <emily.faccin@gmail.com>
2021-07-14 06:45:20 -0300

    changes: product and category factories to unique name

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    name { FFaker::Food.ingredient }
+    sequence(:name) { |n| "category#{n}" }
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    name { FFaker::Food.vegetable }
+    sequence(:name) { |n| "product#{n}" }
     category
   end
 end
