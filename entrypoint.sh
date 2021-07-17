e6026ea5213805984aff98f2b1422139ac9a7680
Emily Faccin <emily.faccin@gmail.com>
2021-07-17 14:53:15 -0300

    fix: db creation on compose-up using entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /farmers_app/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle exec rails db:prepare
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
