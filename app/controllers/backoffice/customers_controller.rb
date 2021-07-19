72f63a187c24b2e2d2f69fe5d783106e51b81831
Emily Faccin <emily.faccin@gmail.com>
2021-07-19 20:57:56 -0300

    add: filter and search feature on customer backoffice

--- app/controllers/backoffice/customers_controller.rb
+++ app/controllers/backoffice/customers_controller.rb
@@ -4,11 +4,19 @@ module Backoffice
 
     def index
       @statuses = Customer.statuses
-      @customers = if params[:status]
-                     Customer.where('status = ?', params[:status])
-                   else
-                     Customer.active
-                   end
+
+      @customers =
+        if params[:status].present? && params[:query].present?
+          Customer.where('status = ? AND name LIKE ? OR email LIKE ?',
+            params[:status], "%#{params[:query]}%", "%#{params[:query]}%")
+        elsif params[:status].present?
+          Customer.where('status = ?', params[:status])
+        elsif params[:query].present?
+          Customer.where('name LIKE ? OR email LIKE ?', "%#{params[:query]}%",
+            "%#{params[:query]}%")
+        else
+          Customer.active
+        end
     end
   end
 end
--- spec/app/views/backoffice/customers/index_filters_spec.rb
+++ spec/app/views/backoffice/customers/index_filters_spec.rb
@@ -6,36 +6,78 @@ feature 'Customers filtering on backoffice index' do
   context 'when admin' do
     include_context 'when admin authenticated'
 
-    let!(:ativo)      { create(:customer, name: 'Active') }
-    let!(:inativo)    { create(:customer, name: 'Blocked', status: :blocked) }
-    let!(:bloqueado)  { create(:customer, name: 'Inactive', status: :inactive) }
-
-    it 'views only active customers when no filter was passed' do
-      visit backoffice_customers_path
-
-      expect(page).to have_content('Active')
-      expect(page).not_to have_content('Inactive')
-      expect(page).not_to have_content('Blocked')
+    let!(:ativo) do
+      create(:customer,
+             name: 'Active',
+             email: 'active@mail.com')
     end
 
-    it 'views inactive customers when filtering inactive' do
-      visit backoffice_customers_path
-      choose 'inactive'
-      click_on 'Buscar'
-
-      expect(page).to have_content('Inactive')
-      expect(page).not_to have_content('Active')
-      expect(page).not_to have_content('Blocked')
+    let!(:inativo) do
+      create(:customer,
+             name: 'Blocked',
+             status: :blocked,
+             email: 'blocked@mail.com')
     end
 
-    it 'views blocked customers when filtering blocked' do
-      visit backoffice_customers_path
-      choose 'blocked'
-      click_on 'Buscar'
+    let!(:bloqueado) do
+      create(:customer,
+             name: 'Inactive',
+             status: :inactive,
+             email: 'inactive@mail.com')
+    end
 
-      expect(page).to have_content('Blocked')
-      expect(page).not_to have_content('Active')
-      expect(page).not_to have_content('Inactive')
+    context 'filtering' do
+      it 'views only active customers when no filter was passed' do
+        visit backoffice_customers_path
+
+        expect(page).to have_content('Active')
+        expect(page).not_to have_content('Inactive')
+        expect(page).not_to have_content('Blocked')
+      end
+
+      it 'views inactive customers when filtering inactive' do
+        visit backoffice_customers_path
+        choose 'inactive'
+        click_on 'Buscar'
+
+        expect(page).to have_content('Inactive')
+        expect(page).not_to have_content('Active')
+        expect(page).not_to have_content('Blocked')
+      end
+
+      it 'views blocked customers when filtering blocked' do
+        visit backoffice_customers_path
+        choose 'blocked'
+        click_on 'Buscar'
+
+        expect(page).to have_content('Blocked')
+        expect(page).not_to have_content('Active')
+        expect(page).not_to have_content('Inactive')
+      end
+    end
+
+    context 'searching' do
+      it 'successfully by name' do
+        visit backoffice_customers_path
+
+        fill_in 'query', with: 'Active'
+        click_on 'Buscar'
+
+        expect(page).to have_content('Active')
+        expect(page).not_to have_content('Inactive')
+        expect(page).not_to have_content('Blocked')
+      end
+
+      it 'successfully by email' do
+        visit backoffice_customers_path
+
+        fill_in 'query', with: 'blocked@mail.com'
+        click_on 'Buscar'
+
+        expect(page).to have_content('Blocked')
+        expect(page).not_to have_content('Active')
+        expect(page).not_to have_content('Inactive')
+      end
     end
   end
 end
