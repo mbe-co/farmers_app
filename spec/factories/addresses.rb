af6c2f21a90ace85941823bf251974d137f1b0af
Emily Faccin <emily.faccin@gmail.com>
2021-07-26 22:18:04 -0300

    add: neighborhood and customer to address factory

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -2,8 +2,10 @@ FactoryBot.define do
   factory :address do
     street { FFaker::AddressBR.street }
     number { rand(1000) }
+    neighborhood { FFaker::AddressBR.neighborhood }
     city { FFaker::AddressBR.city }
     state { FFaker::AddressBR.state }
     zipcode { FFaker::AddressBR.zip_code }
+    customer
   end
 end
