8324799039f4fabc880e9df51c8d0e3a1e482fe2
Emily Faccin <emily.faccin@gmail.com>
2021-07-30 23:16:28 -0300

    refactor: address update using only find_or_initialize

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,21 +1,13 @@
 class AddressController < ApplicationController
   before_action :authenticate_customer!
-  before_action :find_address, only: [:edit, :update]
+  before_action :find_or_initialize_address, only: [:edit, :update]
 
   def edit
   end
 
   # PATCH
   def update
-    @address.tap do |obj|
-      obj.zipcode = address_params[:zipcode]
-      obj.street = address_params[:street]
-      obj.number = address_params[:number]
-      obj.complement = address_params[:complement]
-      obj.neighborhood = address_params[:neighborhood]
-      obj.city = address_params[:city]
-      obj.state = address_params[:state]
-    end
+    @address.attributes = address_params
 
     if @address.save
       redirect_to profile_address_path, notice: t('messages.success_on_edit')
@@ -32,7 +24,7 @@ class AddressController < ApplicationController
       .permit(:zipcode, :street, :number, :complement, :neighborhood, :city, :state)
   end
 
-  def find_address
-    @address = Address.find_or_initialize_by(customer_id: current_customer.id)
+  def find_or_initialize_address
+    @address ||= Address.find_or_initialize_by(customer_id: current_customer.id)
   end
 end
