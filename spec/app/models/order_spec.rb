9a5e509bcb15b8ff6458fd390b29b8c44fb8e4cd
Fernanda Andrade <fernandaao@gmail.com>
2021-08-09 07:33:27 -0300

    adding model and validations order_product

--- spec/app/models/order_spec.rb
+++ spec/app/models/order_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   context 'validations' do
     it { is_expected.to belong_to(:customer) }
+    it { is_expected.to have_many(:order_products) }
+    it { is_expected.to have_many(:products) }
     it { is_expected.to validate_presence_of(:purchase_date) }
     it { is_expected.to validate_presence_of(:due_date) }
   end
--- spec/app/models/product_spec.rb
+++ spec/app/models/product_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Product, type: :model do
 
   context 'validations' do
     it { is_expected.to belong_to(:category) }
+    it { is_expected.to have_many(:order_products) }
+    it { is_expected.to have_many(:orders) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to have_db_index(:name) }
