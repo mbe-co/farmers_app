1e23ac26b00c884f4b1157ab11adbe95aa591d2d
Fernanda Andrade <fernandaao@gmail.com>
2021-08-10 07:26:31 -0300

    fixing address test

--- spec/app/models/address_spec.rb
+++ spec/app/models/address_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe Address, type: :model do
     it { is_expected.to validate_presence_of(:neighborhood) }
     it { is_expected.to validate_presence_of(:city) }
     it { is_expected.to validate_presence_of(:state) }
-    it { is_expected.to validate_presence_of(:cep) }
+    it { is_expected.to validate_presence_of(:zipcode) }
   end
 end
